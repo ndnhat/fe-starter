53b34cd2ef8fc6b52bdf0e8965ad1cbdfbe9155e
Nhat Nguyen <ndnhat@hotmail.com>
2015-08-19 19:44:44 -0600

    add sass processing

--- app/components/auth/login.js
+++ app/components/auth/login.js
@@ -1,6 +1,8 @@
 'use strict';
 import React from 'react';
 
+import '../../styles/login.scss';
+
 class Login extends React.Component {
 	constructor (props) {
 		super(props);
@@ -15,7 +17,7 @@ class Login extends React.Component {
 
 	render() {
 		return (
-			<div className="col-md-2">
+			<div className="login col-md-2 col-md-offset-5">
 				{!this.state.valid && (
 					<div className="alert alert-danger" role="alert">
 					  Your email or password is invalid.
@@ -23,14 +25,14 @@ class Login extends React.Component {
 				)}
 				<form role="login" onSubmit={this.handleSubmit}>
 				  <div className="form-group">
-				    <label for="email">Email</label>
+				    <label htmlFor="email">Email</label>
 				    <input type="email" className="form-control" id="email" placeholder="Email" />
 				  </div>
 				  <div className="form-group">
-				    <label for="password">Password</label>
+				    <label htmlFor="password">Password</label>
 				    <input type="password" className="form-control" id="password" placeholder="Password" />
 				  </div>
-				  <button type="submit" className="btn btn-default">Submit</button>
+				  <button type="submit" className="btn btn-primary">Submit</button>
 				</form>
 			</div>
 		);
